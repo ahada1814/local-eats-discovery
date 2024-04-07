3a3f62d43a821922f503a4f6ca43046dcbee9f3a
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-04-07 12:52:16 +0600

    the form pages is intially created

--- src/pages/SignIn/SignInForm.jsx
+++ src/pages/SignIn/SignInForm.jsx
@@ -1,47 +1,44 @@
-import { Formik } from "formik";
+import { Field, Formik } from "formik";
 
 const SignInForm = () => {
+  const valueViewer = (val) => {
+    console.log(val);
+  };
+
   return (
     <Formik
       initialValues={{ email: "", password: "" }}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
+          valueViewer(values);
           setSubmitting(false);
         }, 400);
       }}
     >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          {errors.email && touched.email && errors.email}
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
+      {({ errors, touched, handleSubmit, isSubmitting }) => (
+        <div className="bg-[#3D83D9] p-8 w-3/12">
+          <form onSubmit={handleSubmit} className="flex flex-col">
+            <h1 className="font-roboto">Email</h1>
+            <Field
+              className="border border-gray-300 bg-transparent rounded-md py-2 px-4 focus:outline-none focus:border-white"
+              type="email"
+              name="email"
+              placeholder="Enter Your Email Address"
+            />
+
+            {errors.email && touched.email && errors.email}
+            <Field
+              className="border border-gray-300 bg-transparent rounded-md py-2 px-4 focus:outline-none focus:border-white"
+              type="password"
+              name="password"
+              placeholder="Enter Your Password"
+            />
+            {errors.password && touched.password && errors.password}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        </div>
       )}
     </Formik>
   );
